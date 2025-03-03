f8fa31b66093e4833203c514aa4e941ca395a02e
bl3ndd <evarzin@viasattech.ru>
2025-03-04 00:15:21 +0400

    i18n

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import logo from "./assets/logo.webp";
 
 import { analytics, AnalyticsEvents } from "./analytics/firebase";
 import { logEvent } from "firebase/analytics";
+import { useTranslation } from 'react-i18next';
+import Metatags from './components/metatags.jsx'
 
 function App() {
+  const { t } = useTranslation();
   const [files, setFiles] = useState([]);
   const [imageSrc, setImageSrc] = useState(null);
   const [processing, setProcessing] = useState(false);
@@ -68,65 +71,68 @@ function App() {
   };
 
   return (
-    <div
-      className="flex flex-col items-center justify-center h-[calc(100vh-80px)] bg-gray-100 w-full"
-      onDrop={(e) => {
-        e.preventDefault();
-        processFiles(e.dataTransfer.files);
-      }}
-      onAbort={(e) => e.preventDefault()}
-      onDragOver={(e) => e.preventDefault()}
-    >
-      <h1 className="text-3xl font-bold mb-2 text-center text-gray-600 hidden ">
-        CutBG
-      </h1>
-      <img src={logo} className="w-1/2 md:w-1/4"></img>
-      <p className="text-gray-600 mb-6 text-center mt-10">
-        Erase image backgrounds for free
-      </p>
-      <div className="chess-background relative flex flex-col items-center justify-center w-84 min-h-64 bg-white border border-gray-300 rounded-xl shadow-md">
-        {imageSrc ? (
-          <img
-            src={imageSrc}
-            alt="Uploaded"
-            className={
-              "w-full h-full object-cover rounded-t-xl" +
-              " " +
-              (processing ? "animate-pulse" : "")
-            }
-          />
-        ) : (
-          <label
-            htmlFor="upload"
-            className="flex flex-col items-center justify-center w-full h-full cursor-pointer"
-            onClick={() => logEvent(analytics, AnalyticsEvents.ButtonRemoveBgClick)}
-          >
-            <div className="flex flex-col items-center">
-              <label
-                className="flex bg-accent text-reversed text-white px-4 py-2 rounded-lg"
-                htmlFor="upload"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6  mr-2"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
+      <>
+          <Metatags t={t} />
+        <div
+            className="flex flex-col items-center justify-center h-[calc(100vh-80px)] bg-gray-100 w-full"
+            onDrop={(e) => {
+              e.preventDefault();
+              processFiles(e.dataTransfer.files);
+            }}
+            onAbort={(e) => e.preventDefault()}
+            onDragOver={(e) => e.preventDefault()}
+        >
+          <h1 className="text-3xl font-bold mb-2 text-center text-gray-600 hidden ">
+            CutBG
+          </h1>
+          <img src={logo} className="w-1/2 md:w-1/4"></img>
+          <p className="text-gray-600 mb-6 text-center mt-10">
+            {t('title')}
+          </p>
+          <div
+              className="chess-background relative flex flex-col items-center justify-center w-84 min-h-64 bg-white border border-gray-300 rounded-xl shadow-md">
+            {imageSrc ? (
+                <img
+                    src={imageSrc}
+                    alt="Uploaded"
+                    className={
+                        "w-full h-full object-cover rounded-t-xl" +
+                " " +
+                (processing ? "animate-pulse" : "")
+              }
+            />
+          ) : (
+            <label
+              htmlFor="upload"
+              className="flex flex-col items-center justify-center w-full h-full cursor-pointer"
+              onClick={() => logEvent(analytics, AnalyticsEvents.ButtonRemoveBgClick)}
+            >
+              <div className="flex flex-col items-center">
+                <label
+                  className="flex bg-accent text-reversed text-white px-4 py-2 rounded-lg"
+                  htmlFor="upload"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 16v1a2 2 0 002 2h12a2 2 0 002-2v-1M8 12l4-4m0 0l4 4m-4-4v12"
-                  />
-                </svg>
-                <div className="cursor-pointer">Start from a photo</div>
-              </label>
-              <p className="text-gray-500 mt-2">Or drop an image here</p>
-            </div>
-            <input
-              type="file"
-              id="upload"
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6  mr-2"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 16v1a2 2 0 002 2h12a2 2 0 002-2v-1M8 12l4-4m0 0l4 4m-4-4v12"
+                    />
+                  </svg>
+                  <div className="cursor-pointer">{t('actionButton')}</div>
+                </label>
+                <p className="text-gray-500 mt-2">{t('subtitle')}</p>
+              </div>
+              <input
+                type="file"
+                id="upload"
               className="hidden"
               onChange={handleFileChange}
             />
@@ -188,13 +194,13 @@ function App() {
         ) : null}
         {completed ? (
           <div className="bg-secondary w-full p-4 rounded-b-lg text-center text-gray-600 text-xs">
-            How is your experience?
+            {t('howIsYourExpirience')}
             <div className="flex flex-row space-x-1 items-center justify-center mt-2">
               <button
                 onClick={() => logEvent(analytics, AnalyticsEvents.ButtonLikeClick)}
                 className="p-2 rounded bg-accent text-reversed w-40 cursor-pointer"
               >
-                I like it!
+                {t('buttonLike')}
               </button>
               <button
                 onClick={() => logEvent(
@@ -203,13 +209,14 @@ function App() {
                 )}
                 className="bg-accent p-2 rounded text-reversed w-40 cursor-pointer"
               >
-                I don't like it
+                {t('buttonDislike')}
               </button>
             </div>
           </div>
         ) : null}
       </div>
     </div>
+      </>
   );
 }
 
---
+++ src/i18n/i18n.js
@@ -0,0 +1,35 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "../../locales/en.json";
+import cn from "../../locales/cn.json";
+import de from "../../locales/de.json";
+import es from "../../locales/es.json";
+import fr from "../../locales/fr.json";
+import hi from "../../locales/hi.json";
+import it from "../../locales/it.json";
+import ja from "../../locales/ja.json";
+import pt from "../../locales/pt.json";
+
+const languages = {
+    en: { translation: en },
+    cn: { translation: cn },
+    de: { translation: de },
+    es: { translation: es },
+    fr: { translation: fr },
+    hi: { translation: hi },
+    it: { translation: it },
+    ja: { translation: ja },
+    pt: { translation: pt },
+}
+
+const userLanguage = navigator.language || navigator.userLanguage
+const selectedLanguage = Object.keys(languages).includes(userLanguage) ? userLanguage : 'en'
+console.log(selectedLanguage)
+i18n.use(initReactI18next).init({
+    resources: languages,
+    lng: selectedLanguage, // Язык по умолчанию
+    fallbackLng: "en",
+    interpolation: { escapeValue: false },
+});
+
+export default i18n;
