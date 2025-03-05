0fc8c29fcca8c9222653f99e5209c8775520c245
Vladimir <numkms@gmail.com>
2025-03-06 01:24:40 +0300

    same translations

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -2,11 +2,12 @@ import './index.css'
 import { StrictMode } from 'react'
 import { hydrateRoot } from 'react-dom/client'
 import App from './App'
+import { I18nextProvider } from 'react-i18next';
 import './i18n/i18n.js';
 
 hydrateRoot(
-
   document.getElementById('root'),
-  <App />
-  ,
+  <I18nextProvider i18n={i18n}>
+    <App />
+  </I18nextProvider>
 )
