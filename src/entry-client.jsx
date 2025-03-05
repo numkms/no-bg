2fa403da2c791923ebbf91779bf2fdefefd42eba
Vladimir <numkms@gmail.com>
2025-03-06 01:30:20 +0300

    Import fixed

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import { hydrateRoot } from 'react-dom/client'
 import App from './App'
 import { I18nextProvider } from 'react-i18next';
-import './i18n/i18n.js';
+import i18n from './i18n/i18n.js';
 
 hydrateRoot(
   document.getElementById('root'),
