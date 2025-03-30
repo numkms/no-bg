c2d0cfec7595c80ff189fedb74a93decdf90e0bb
Vladimir <numkms@gmail.com>
2025-03-30 05:16:10 +0300

    Fixed language by url on SSR

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,10 +1,16 @@
 import { renderToString } from 'react-dom/server'
 import App from './apps/App.jsx'
-import './i18n/i18n.js';
+import {getLanguagesList} from './i18n/i18n.js';
 /**
  * @param {string} _url
  */
 export function render(_url, lang) {
+  let languages = getLanguagesList();
+  console.log(languages, _url)
+  if (languages.includes(_url)) {
+    lang = _url 
+    console.log(lang)
+  }
   const html = renderToString(
       <App lang={lang} />
   )
