3887a7afbaa168172ded37e3325b5a48891aa884
Vladimir <numkms@gmail.com>
2025-03-30 05:25:49 +0300

    how to use

--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -1,7 +1,7 @@
 export default ({t}) => {
     return (
         <div className="mt-6 text-center">
-            <h2>How to use?</h2> 
+            <h2>{t('how_to_use')}</h2> 
             {t('tutorial').split('\n').filter(e => e.length > 0).map((line, index) => (
                 <div className="tutorial-element flex justify-center  items-center w-84  sm:w-[500px] pl-1 pr-1 pt-1 pb-1 bg-white rounded-md min-h-10 shadow-md overflow-hidden mt-1 text-xs text-neutral-600" key={index}>
                     {line}
