761bb0018288796a89e357ab521aa9e1b883376f
Voooooooooooooooova <numkms@gmail.com>
2025-10-25 12:53:35 +0300

    feedback support

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,46 +1,52 @@
-import {logEvent} from "firebase/analytics";
-import {analytics, AnalyticsEvents} from "../analytics/firebase.js";
+import { logEvent } from "firebase/analytics";
+import { analytics, AnalyticsEvents } from "../analytics/firebase.js";
 import React from "react";
+import { BuyMeCoffee } from "./BuyMeCoffee.jsx";
 
-export const Feedback = ({isCompleted, isFeedbackSent, setIsFeedbackSent, t}) => {
+export const Feedback = ({
+  isCompleted,
+  isFeedbackSent,
+  setIsFeedbackSent,
+  t,
+}) => {
+  const onLikeClick = () => {
+    setIsFeedbackSent(true);
+    logEvent(analytics, AnalyticsEvents.ButtonLikeClick);
+  };
 
-    const onLikeClick = () => {
-        setIsFeedbackSent(true);
-        logEvent(analytics, AnalyticsEvents.ButtonLikeClick)
-    }
+  const onDislikeClick = () => {
+    setIsFeedbackSent(true);
+    logEvent(analytics, AnalyticsEvents.ButtonDislikeClick);
+  };
 
-    const onDislikeClick = () => {
-        setIsFeedbackSent(true);
-        logEvent(analytics, AnalyticsEvents.ButtonDislikeClick,)
-    }
-
-    return (
-        isCompleted ? (
-            <div className="bg-secondary w-full mt-4 p-4 rounded-lg text-center text-gray-600 text-xs">
-                {!isFeedbackSent ? (<div>
-                    {t('howIsYourExpirience')}
-                    <div className="flex flex-row space-x-1 items-center justify-center mt-2">
-                        <button
-                            onClick={() => onLikeClick()}
-                            className="p-2 rounded bg-accent text-reversed w-38 cursor-pointer"
-                            aria-label={t('buttonLike')}
-                        >
-                            {t('buttonLike')}
-                        </button>
-                        <button
-                            onClick={() => onDislikeClick()}
-                            className="bg-accent p-2 rounded text-reversed w-38 cursor-pointer"
-                            aria-label={t('buttonDislike')}
-                        >
-                            {t('buttonDislike')}
-                        </button>
-                    </div>
-                </div>) : (
-                    <div>
-                        {t('thanksForFeedback')}
-                    </div>
-                )}
-            </div>
-        ) : null
-    )
-}
+  return isCompleted ? (
+    <div className="bg-secondary w-full mt-4 p-4 rounded-lg text-center text-gray-600 text-xs">
+      {!isFeedbackSent ? (
+        <div>
+          {t("howIsYourExpirience")}
+          <div className="flex flex-row space-x-1 items-center justify-center mt-2">
+            <button
+              onClick={() => onLikeClick()}
+              className="p-2 rounded bg-accent text-reversed w-38 cursor-pointer"
+              aria-label={t("buttonLike")}
+            >
+              {t("buttonLike")}
+            </button>
+            <button
+              onClick={() => onDislikeClick()}
+              className="bg-accent p-2 rounded text-reversed w-38 cursor-pointer"
+              aria-label={t("buttonDislike")}
+            >
+              {t("buttonDislike")}
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          {t("thanksForFeedback")}
+          <BuyMeCoffee t={t}></BuyMeCoffee>
+        </div>
+      )}
+    </div>
+  ) : null;
+};
